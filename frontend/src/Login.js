334a5d7412c03e0fda52b56fee7835b86ce068c6
Ethan Dang <nguyen180904@gmail.com>
2023-11-02 13:52:47 +1100

    Change Validation directory

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Validation from './LoginValidation';
+import Validation from './validation/LoginValidation';
 import Navbar from './NavBar'; 
 
 function Login() {
